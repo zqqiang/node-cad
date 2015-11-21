219009b60f42e1d143f0d1ec1eb98a7a022375cf
ZhaoqingQiang <qiangzhaoqing@gmail.com>
2015-11-20 17:28:25 -0800

    modify install.js

--- install.js
+++ install.js
@@ -1,19 +1,43 @@
 // var debug = require('debug')('set-env');
 var exec = require('child_process').exec;
 
-process.env.CL = __dirname + '\\oce-0.17\\include\\oce';
-process.env.LINK = '/LIBPATH:' + __dirname + '\\oce-0.17\\Win32\\lib';
-process.env.PATH += (';' + __dirname + '\\oce-0.17\\Win32\\bin');
+var CL = __dirname + '\\oce-0.17\\include\\oce';
+var LINK = __dirname + '\\oce-0.17\\Win32\\lib';
+var PATH = __dirname + '\\oce-0.17\\Win32\\bin';
 
-// debug(process.env.CL);
-// debug(process.env.LINK);
-// debug(process.env.PATH);
-
-console.log('CL [%s]', process.env.CL);
-console.log('LINK [%s]', process.env.LINK);
-console.log('PATH [%s]', process.env.PATH);
+console.log('CL [%s]', CL);
+console.log('LINK [%s]', LINK);
+console.log('PATH [%s]', PATH);
 
 exec('git clone https://github.com/erossignon/node-occ ./node_modules/node-occ', function(error, stdout, stderr) {
+	console.log('stdout: ' + stdout);
+	console.log('stderr: ' + stderr);
+	if (error !== null) {
+		console.log('exec error: ' + error);
+	}
+});
+
+// >SET CL=/Ic:\OCE-0.17\include\oce
+// >SET LINK=/LIBPATH:c:\OCE-0.17\Win32\lib
+// >PATH c:\OCE-0.17\Win32\bin;%PATH%
+
+exec('SET CL=/I' + CL, function(error, stdout, stderr) {
+	console.log('stdout: ' + stdout);
+	console.log('stderr: ' + stderr);
+	if (error !== null) {
+		console.log('exec error: ' + error);
+	}
+});
+
+exec('SET LINK=/LIBPATH:' + LINK, function(error, stdout, stderr) {
+	console.log('stdout: ' + stdout);
+	console.log('stderr: ' + stderr);
+	if (error !== null) {
+		console.log('exec error: ' + error);
+	}
+});
+
+exec('PATH ' + PATH + ';%PATH%', function(error, stdout, stderr) {
 	console.log('stdout: ' + stdout);
 	console.log('stderr: ' + stderr);
 	if (error !== null) {
