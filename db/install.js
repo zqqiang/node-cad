712592ef222d0856613b09b442ed698692c01617
ZhaoqingQiang <qiangzhaoqing@gmail.com>
2016-01-26 09:16:12 -0800

    add cadfix

--- db/install.js
+++ db/install.js
@@ -7,5 +7,22 @@ var copy = function copy(src, dst) {
 	})
 };
 
-copy('./config.pl', '../postgresql-9.5rc1/src/tools/msvc/config.pl');
-copy('./VSObjectFactory.pm', '../postgresql-9.5rc1/src/tools/msvc/VSObjectFactory.pm');
+copy('./config.pl', '../postgresql-9.5.0/src/tools/msvc/config.pl');
+copy('./VSObjectFactory.pm', '../postgresql-9.5.0/src/tools/msvc/VSObjectFactory.pm');
+
+fs.access('../postgresql-9.5.0/contrib/cadfix/', function(err) {
+	if (err) {
+		console.log('../postgresql-9.5.0/contrib/cadfix/ doesn\'t exist! create it.');
+		fs.mkdir('../postgresql-9.5.0/contrib/cadfix/', function(err) {
+			if (err) {
+				console.log('mkdir failed!');
+			} else {
+				copy('./cadfix/Makefile', '../postgresql-9.5.0/contrib/cadfix/Makefile');
+				copy('./cadfix/cad.c', '../postgresql-9.5.0/contrib/cadfix/cad.c');
+			}
+		})
+	} else {
+		copy('./cadfix/Makefile', '../postgresql-9.5.0/contrib/cadfix/Makefile');
+		copy('./cadfix/cad.c', '../postgresql-9.5.0/contrib/cadfix/cad.c');
+	}
+});
