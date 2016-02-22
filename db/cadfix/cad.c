6f5d83ba08db3c873b5ddc17be29b5ec4cc63327
ZhaoqingQiang <qiangzhaoqing@gmail.com>
2016-02-22 15:08:35 -0800

    rm cc call

--- db/cadfix/cad.c
+++ db/cadfix/cad.c
@@ -32,23 +32,23 @@ Datum cadinit(PG_FUNCTION_ARGS) {
 
   STEPControl_Reader aReader;
 
-  Interface_Static::SetCVal("xstep.cascade.unit", "mm");
-  Interface_Static::SetIVal("read.step.nonmanifold", 1);
-  Interface_Static::SetIVal("read.step.product.mode", 1);
-  Interface_Static::SetRVal("read.precision.val", 0.01);
+  // Interface_Static::SetCVal("xstep.cascade.unit", "mm");
+  // Interface_Static::SetIVal("read.step.nonmanifold", 1);
+  // Interface_Static::SetIVal("read.step.product.mode", 1);
+  // Interface_Static::SetRVal("read.precision.val", 0.01);
 
-  if (aReader.ReadFile(_filename.c_str()) != IFSelect_RetDone) {
-    puts("LOG: open model failed");
-  } else {
-    puts("LOG: open model success");
+  // if (aReader.ReadFile(_filename.c_str()) != IFSelect_RetDone) {
+  //   puts("LOG: open model failed");
+  // } else {
+  //   puts("LOG: open model success");
 
     // todo: better calculate line number method?
-    TopoDS_Shape aResShape;
-    TopExp_Explorer expEdge(aResShape, TopAbs_EDGE);
-    while (expEdge.More()) {
-      n++;
-      expEdge.Next();
-    }
+    // TopoDS_Shape aResShape;
+    // TopExp_Explorer expEdge(aResShape, TopAbs_EDGE);
+    // while (expEdge.More()) {
+    //   n++;
+    //   expEdge.Next();
+    // }
 
     printf("there are %d lines\n", n);
     FILE* fs = fopen("/home/zhibin/logs/edgenumber.csv", "a+");
